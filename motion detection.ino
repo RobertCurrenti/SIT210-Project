e370335641d423633a206539003cee859ad340dc
Robert Currenti <82944578+RobertCurrenti@users.noreply.github.com>
2022-06-08 16:37:41 +1000

    Add files via upload

---
+++ motion detection.ino
@@ -0,0 +1,30 @@
+bool just_published = false;
+int publish_count = 0;
+
+void setup() {
+    pinMode(D2, INPUT);
+    
+    // wait 2 minutes before motion detection is allowed
+    // so user does not set off device by acident
+    delay(120 * 1000);
+}
+
+void loop() {
+
+    // wait 1 mins before publishing again
+    // can only publish 3 times, so not to clog users inbox
+    if (just_published == true and publish_count < 3)
+    {
+        delay(60 * 1000);
+        just_published = false;
+    }
+    
+    // if motion is detected publish event to trigger email being sent
+    if (digitalRead(D2) == HIGH and just_published == false)
+    {
+        Particle.publish("event", "triggered", PUBLIC);
+        just_published = true;
+        publish_count += 1;
+    }
+    
+}
